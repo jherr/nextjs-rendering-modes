1f1ab7b175545f354f83b5ef15eba49dc89705da
Jack Herrington <jherr@pobox.com>
2022-03-07 21:13:31 -0800

    Update revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,3 +1,7 @@
+// I didn't follow the security pattern suggested in the NextJS documentation but you should:
+//
+// https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
+
 export default async function handler(req, res) {
   for (const url of req.body ?? []) {
     await res.unstable_revalidate(url);
